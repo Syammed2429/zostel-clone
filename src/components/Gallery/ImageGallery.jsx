cc1176b4c9ec4e1d8f0384dd277095620dd9c213
Rishabh Anand <rishabhanand02@gmail.com>
2021-12-18 21:09:51 +0530

    Gallery Updated

--- src/components/Gallery/ImageGallery.jsx
+++ src/components/Gallery/ImageGallery.jsx
@@ -1,55 +1,41 @@
-import axios from "axios";
-import { useState, useEffect } from "react"
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import "./ImageGallery.css"
+import { Link } from "react-router-dom";
 
 export const ImagesGallery = () => {
 
     const images = [
         {
-          original: 'https://picsum.photos/id/1018/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1018/250/150/',
+            original: 'https://img.cdn.zo.xyz/zostel/gallery/images/kR55bIDpRYC-2wYqnlT6jA/manali-20201112102605.jpg?w=1280',
+            thumbnail: 'https://img.cdn.zo.xyz/zostel/gallery/images/kR55bIDpRYC-2wYqnlT6jA/manali-20201112102605.jpg?w=1280',
         },
         {
-          original: 'https://picsum.photos/id/1015/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1015/250/150/',
+            original: 'https://img.cdn.zo.xyz/zostel/gallery/images/2B_i6AW9RTaNmY6d-2Bcmw/manali-board-20201105110533.jpg?w=1280',
+            thumbnail: 'https://img.cdn.zo.xyz/zostel/gallery/images/2B_i6AW9RTaNmY6d-2Bcmw/manali-board-20201105110533.jpg?w=1280',
         },
         {
-          original: 'https://picsum.photos/id/1019/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1019/250/150/',
+            original: 'https://img.cdn.zo.xyz/zostel/gallery/images/15R001HDS2Cc_wCv97eAPQ/manali-garden-cafe-20201105110535.jpg?w=1280',
+            thumbnail: 'https://img.cdn.zo.xyz/zostel/gallery/images/15R001HDS2Cc_wCv97eAPQ/manali-garden-cafe-20201105110535.jpg?w=1280',
         },
-      ];
-      
+        {
+            original: 'https://img.cdn.zo.xyz/zostel/gallery/images/GkyGaeFwQnilVex_Cu_eHQ/manali-wall-art-20201105110536.jpg?w=1280',
+            thumbnail: 'https://img.cdn.zo.xyz/zostel/gallery/images/GkyGaeFwQnilVex_Cu_eHQ/manali-wall-art-20201105110536.jpg?w=1280',
+        },
+        {
+            original: 'https://img.cdn.zo.xyz/zostel/gallery/images/DOgsYSjnSJGZMX-8JOSyDg/manali_zostel_garden_cafe-20201105110548.jpg?w=1280',
+            thumbnail: 'https://img.cdn.zo.xyz/zostel/gallery/images/DOgsYSjnSJGZMX-8JOSyDg/manali_zostel_garden_cafe-20201105110548.jpg?w=1280',
+        },
 
-    // const [images, setImages] = useState(null);
-
-    // useEffect(() => {
-    //     let shouldCancel = false;
-    //     const call = async () => {
-    //         const response = await axios.get("https://www.npmjs.com/package/react-responsive-carousel");
-
-    //         if (!shouldCancel && response.data && response.data.length > 0) {
-    //             setImages(
-    //                 response.data.map(url => ({
-    //                     original: `${url}=w1024`,
-    //                     thumbnail: `${url}=w100`
-    //                 }))
-    //             );
-    //         }
-    //     };
-
-    //     call();
-    //     return () => (shouldCancel = true);
-
-    // }, []);
-
+    ];
 
     return (
         <>
-            <div className="cancel-btn">
-                <img src="x-lg.svg" alt="cancel" />
-            </div>
+            <Link to="/booking">
+                <div className="cancel-btn">
+                    <img src="x-lg.svg" alt="cancel" />
+                </div>
+            </Link>
             <ImageGallery items={images} />;
         </>
     )
