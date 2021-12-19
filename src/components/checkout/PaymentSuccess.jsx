f96b4ffc1683b6a8854f7a32c13b7f090ca2cbb5
Dada Khalandar <kerry.blig12@gmail.com>
2021-12-19 17:28:04 +0530

    increaded success timeout

--- src/components/checkout/PaymentSuccess.jsx
+++ src/components/checkout/PaymentSuccess.jsx
@@ -9,14 +9,14 @@ const PaymentSuccess = () => {
     React.useEffect(() => {
         setTimeout(() => {
             return history.push("/")
-        }, 4000)
+        }, 7000)
     }, [history]);
 
     return (
         <div className="container text-center">
             <img
                 className=" img-fluid"
-                style={{width:"40%"}}
+                style={{ width: "40%" }}
                 src={successMsg} alt="success" />
             <h2 className="my-3 h1 text-success">Payment Successful!</h2>
 
